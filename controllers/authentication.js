62541c466d0ab06aa7339d1f5b8cbfd16eb66e15
gogolab <m4ttek@gmail.com>
2018-10-26 14:00:16 +0200

    added auth controller

---
+++ controllers/authentication.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+exports.signup = function(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // See if the user with the given email exists
+    User.findOne({ email: email }, (err, existingUser) => {
+        if (err) {
+            return next(err);
+        }
+
+        // If the user with email exist, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: "Email is in use" });
+        }
+
+        // If the user with email does NOT exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(err => {
+            if (err) {
+                return next(err);
+            }
+
+            // Respond to request indicating the user was created
+            res.json(user);
+        });
+    });
+};
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const router = require("./router");
 
 const app = express();
+
+// DB setup
+mongoose.connect("mongodb://localhost:27017/auth");
 
 // App setup
 app.use(morgan("combined"));
