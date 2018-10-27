cec31946391104731baabb4e59e1b2b67dc4fd27
gogolab <m4ttek@gmail.com>
2018-10-27 17:52:38 +0200

    added guarding routes by passport

--- router.js
+++ router.js
@@ -1,7 +1,12 @@
+const passport = require("passport");
+
 const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = function(app) {
-    app.get("/", function(req, res, next) {
+    app.get("/", requireAuth, function(req, res, next) {
         res.send(["diana prince", "clark kent", "bruce wayne"]);
     });
     app.post("/signup", Authentication.signup);
