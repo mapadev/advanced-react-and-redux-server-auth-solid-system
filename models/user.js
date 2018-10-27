70b6d2843ecaae4ce02da1baf93bc0a9beee985e
gogolab <m4ttek@gmail.com>
2018-10-27 14:23:02 +0200

    added password hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 
 const Schema = mongoose.Schema;
 
@@ -6,6 +7,26 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
+});
+
+// On save hook, encrypt password
+userSchema.pre("save", function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 // Create the model class
--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 const Authentication = require("./controllers/authentication");
 
 module.exports = function(app) {
-    // app.get("/", function(req, res, next) {
-    //     res.send(["diana prince", "clark kent", "bruce wayne"]);
-    // });
+    app.get("/", function(req, res, next) {
+        res.send(["diana prince", "clark kent", "bruce wayne"]);
+    });
     app.post("/signup", Authentication.signup);
 };
