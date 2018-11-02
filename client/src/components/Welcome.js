7bdd242707038e2405df365484e98055946801c0
mapadev <m4ttek@gmail.com>
2018-11-02 19:07:21 +0100

    added basic components and router

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => {
+    return <h3>Welcome! Sign In or Sing Up.</h3>;
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+        <App>
+            <Route path="/" exact component={Welcome} />
+            <Route path="/signup" component={Signup} />
+        </App>
+    </Router>,
+    document.getElementById("root")
+);
