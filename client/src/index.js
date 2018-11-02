870b6499b8b869e0ecea4ae8df9535beaa582d94
mapadev <m4ttek@gmail.com>
2018-11-02 19:21:09 +0100

    added redux basics

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
+import reducers from "./reducers";
 
 ReactDOM.render(
-    <Router>
-        <App>
-            <Route path="/" exact component={Welcome} />
-            <Route path="/signup" component={Signup} />
-        </App>
-    </Router>,
+    <Provider store={createStore(reducers, {})}>
+        <Router>
+            <App>
+                <Route path="/" exact component={Welcome} />
+                <Route path="/signup" component={Signup} />
+            </App>
+        </Router>
+    </Provider>,
     document.getElementById("root")
 );
