29c713273e9d3283a9727f1927a871587f6f5448
mapadev <m4ttek@gmail.com>
2018-11-03 19:41:31 +0100

    added auth async action creator and updated auth reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,6 +7,11 @@ import axios from "axios";
 
 // Because we destructure props just to make another object with those props,
 // we can just pass props directly:
-export const signup = formProps => dispatch => {
-    axios.post("http://localhost:3090/signup", formProps);
+export const signup = formProps => async dispatch => {
+    const response = await axios.post(
+        "http://localhost:3090/signup",
+        formProps
+    );
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
 };
