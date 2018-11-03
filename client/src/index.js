b3c630043ed9a05f165ebd59af31534951757b04
mapadev <m4ttek@gmail.com>
2018-11-03 14:26:43 +0100

    added form submit handler and added redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
 import reducers from "./reducers";
 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
         <Router>
             <App>
                 <Route path="/" exact component={Welcome} />
